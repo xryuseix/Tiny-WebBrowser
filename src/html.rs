a0d7e6f0fa43cfdc63be7219226bcc8f0480f8a2
xryuseix <ryusei143.shootingstar@gmail.com>
2021-10-11 15:16:58 +0900

    feat: add attributes parser

--- src/html.rs
+++ src/html.rs
@@ -1,7 +1,7 @@
 use crate::dom::{AttrMap, Element, Node};
 use combine::error::ParseError;
-use combine::parser::char::char;
-use combine::{parser, Parser, Stream};
+use combine::parser::char::{char, letter, newline, space};
+use combine::{between, many, many1, parser, satisfy, Parser, Stream};
 
 /// `attribute` consumes `name="value"`.
 fn attribute<Input>() -> impl Parser<Input, Output = (String, String)>
@@ -9,8 +9,18 @@ where
     Input: Stream<Token = char>,
     Input::Error: ParseError<Input::Token, Input::Range, Input::Position>,
 {
-    todo!("you need to implement this combinator");
-    (char(' ')).map(|_| ("".to_string(), "".to_string()))
+    (
+        many1::<String, _, _>(letter()), // まずは属性の名前を何文字か読む
+        many::<String, _, _>(space().or(newline())), // 空白と改行を読み飛ばす
+        char('='),                       // = を読む
+        many::<String, _, _>(space().or(newline())), // 空白と改行を読み飛ばす
+        between(
+            char('"'),
+            char('"'),
+            many1::<String, _, _>(satisfy(|c: char| c != '"')),
+        ), // 引用符の間の、引用符を含まない文字を読む
+    )
+        .map(|v| (v.0, v.4)) // はじめに読んだ属性の名前と、最後に読んだ引用符の中の文字列を結果として返す
 }
 
 /// `attributes` consumes `name1="value1" name2="value2" ... name="value"`
@@ -19,8 +29,7 @@ where
     Input: Stream<Token = char>,
     Input::Error: ParseError<Input::Token, Input::Range, Input::Position>,
 {
-    todo!("you need to implement this combinator");
-    (char(' ')).map(|_| AttrMap::new())
+    many((attribute(), many::<String, _, _>(space().or(newline()))).map(|v| v.0))
 }
 
 /// `open_tag` consumes `<tag_name attr_name="attr_value" ...>`.
